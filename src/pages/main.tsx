cd4ac171464c55018b3ba0c2a98101be2d0cbab0
Vladimirs Bičkovs <v.k.bickovs@student.tudelft.nl>
2025-02-15 21:21:15 +0100

    Base template.

---
+++ src/pages/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from "./home";
+import '../css/main.css';
+
+function Main() {
+	return (
+		<div className="bg">
+			<div className="main">
+				<Home />
+			</div>
+		</div>
+	);
+}
+
+export default Main;
