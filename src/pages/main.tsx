17dfa414cb63111dab72fd6daa79987ec67d3262
Vladimirs Bičkovs <v.k.bickovs@student.tudelft.nl>
2025-02-16 01:08:54 +0100

    Speed improvements.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,49 +1,56 @@
 import React from 'react';
-import Home from "./home";
 import Link from "../components/Link";
+
+import Home from "./home";
+import Education from "./education";
+
 import '../css/main.css';
 
 function Main() {
-
-	// get page from url
-	let component = null;
+	let activePage;
 	const url = window.location.pathname;
+
 	switch (url) {
 		case "/education":
-			component = <Home/>;
+			activePage = "education";
 			break;
 		case "/research":
-			component = <Home/>;
+			activePage = "research";
 			break;
 		case "/gallery":
-			component = <Home/>;
+			activePage = "gallery";
 			break;
 		default:
-			component = <Home/>;
+			activePage = "home";
 	}
 
 	return (
 		<div className="bg">
 			<div className="main">
-
 				<h2>Vladimir Kiril Bickov</h2>
 
 				<div className="menu">
-					<Link link="/">
-						Home
-					</Link>
-					<Link link="/education">
-						Education
-					</Link>
-					<Link link="/research">
-						Research
-					</Link>
-					<Link link="/gallery">
-						Gallery
-					</Link>
+					<Link link="/">Home</Link>
+					<Link link="/education">Education</Link>
+					<Link link="/research">Research</Link>
+					<Link link="/gallery">Gallery</Link>
 				</div>
 
-				{component}
+				<div style={{ display: activePage === "home" ? "block" : "none" }}>
+					<Home />
+				</div>
+
+				<div style={{ display: activePage === "education" ? "block" : "none" }}>
+					<Education />
+				</div>
+
+				<div style={{ display: activePage === "research" ? "block" : "none" }}>
+					<Home />
+				</div>
+
+				<div style={{ display: activePage === "gallery" ? "block" : "none" }}>
+					<Home />
+				</div>
 			</div>
 		</div>
 	);
