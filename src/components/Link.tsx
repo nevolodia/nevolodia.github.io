e6218ae8d1518f9c0ba4915218175cbd7419144c
Vladimirs Bičkovs <v.k.bickovs@student.tudelft.nl>
2025-02-16 01:23:07 +0100

    Link updated to be clicked on mouse down (not release).

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import '../css/link.css';
 
 function Link(props: { link: string, children: React.ReactNode }) {
+	const handleMouseDown = () => {
+		window.location.href = props.link;
+	};
+
 	return (
-		<a className="link" href={props.link}>
+		<a
+			className="link" href={props.link}
+			onMouseDown={handleMouseDown}
+		>
 			{props.children}
 		</a>
 	);
