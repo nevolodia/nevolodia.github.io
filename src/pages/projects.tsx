8c549284845b0b88d2a6202f6c9eb9ec0605ba42
Vladimirs Bičkovs <v.k.bickovs@student.tudelft.nl>
2025-02-16 13:52:29 +0100

    Projects page added.

---
+++ src/pages/projects.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Projects() {
+	return (
+		<div>
+			<p>
+				Here are some of my projects:
+
+				<ol>
+					<li>
+						An online shop <a href="https://binatec.eu/" target="_blank">
+							https://binatec.eu/
+						</a> for European household chemistry company.
+						Made with WordPress, WooCommerce, and a bit of love.
+					</li>
+				</ol>
+			</p>
+		</div>
+	);
+}
+
+export default Projects;
