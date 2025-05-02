3e0f438426d3de736a331d7ff9de127dd01f5ba8
Vladimirs Bičkovs <v.k.bickovs@student.tudelft.nl>
2025-05-02 18:43:07 +0200

    Some cool stuff.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,31 @@ function Home() {
 	return (
 		<div>
 			<p>
-				My name is Vladimir, and I am doing coding and some other stuff.
+				My name is Vladimir, and I am doing some Computer Science and Math things.
+
+				<br/>
+				<br/>
+
+				Some achievements of mine:
+
+				1. Wrote some cool stuff in university, like a JIT compiler for Brainfuck
+				in x86 Assembly or a jq implementation in Haskell.
+
+				2. Won 4th place in the algo agent tower defense competition Terminal,
+				organized by Citadel Securities trading company, competing against
+				teams from top European universities.
+
+				3. Won 1st place in the Port Transition challenge by the Port of Rotterdam
+				and Erasmus University Rotterdam. Developed an ML solution to predict
+				distributions in the port.
+
+				4. Got 1st place in the Latvian Open Olympiad in Economics 2024, 2nd place
+				in the National Olympiad in Economics 2023, and 3rd place in the Latvian Linguistics
+				Olympiad 2024.
+
+				<br/>
+				<br/>
+
 				Check other pages for more information. :)
 			</p>
 		</div>
