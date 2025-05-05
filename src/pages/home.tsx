4e22c5f423cfe524e2a41ccad79c0dfd92d879f4
Vladimirs Bičkovs <v.k.bickovs@student.tudelft.nl>
2025-05-05 02:36:31 +0200

    Home page updated.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import Image from "../components/Image";
+import GalleryComponent from "../components/GalleryComponent";
 
 function Home() {
+	const brainfuck_x86 = require('../resources/images/home/brfck_x86_image.png');
+	const ads = require('../resources/images/home/ads.jpg');
+	const rott = require('../resources/images/home/rott.jpg');
+	const bunq = require('../resources/images/home/bunq.jpg');
+
 	return (
 		<div>
 			<p>
@@ -14,8 +21,9 @@ function Home() {
 				<br/>
 				<br/>
 
-				1. Wrote some cool stuff in university, like a JIT compiler for Brainfuck
-				in x86 Assembly or a jq implementation in Haskell.
+				1. Wrote JIT compiler for Brainfuck in x86 Assembly,
+				a jq implementation in Haskell,
+				and other cool stuff.
 
 				<br/>
 				<br/>
@@ -27,14 +35,19 @@ function Home() {
 				<br/>
 				<br/>
 
-				3. Won 1st place in the Port Transition challenge by the Port of Rotterdam
-				and Erasmus University Rotterdam. Developed an ML solution to predict
-				distributions in the port.
+				3. Won 2nd place in hackathon by bunq bank. Developed saluting for importing
+				or defining in UI financial operation and then analyzing and running them.
 
 				<br/>
 				<br/>
 
-				4. Got 1st place in the Latvian Open Olympiad in Economics 2024, 2nd place
+				4. Won 1st place in the Port Transition challenge by the Port of Rotterdam.
+				Developed an ML solution to predict distributions in the port.
+
+				<br/>
+				<br/>
+
+				5. Got 1st place in the Latvian Open Olympiad in Economics 2024, 2nd place
 				in the National Olympiad in Economics 2023, and 3rd place in the Latvian Linguistics
 				Olympiad 2024.
 
@@ -42,6 +55,19 @@ function Home() {
 				<br/>
 
 				Check other pages for more information. :)
+
+				<br/>
+				<br/>
+
+				Anc check out the gallery of previously described achievements:
+
+				<br/>
+				<br/>
+
+				<Image link={brainfuck_x86}/>
+				<Image link={bunq}/>
+				<Image link={ads}/>
+				<Image link={rott}/>
 			</p>
 		</div>
 	);
