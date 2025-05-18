54d815a4672c7398b6a4225d5563e10a114d587c
nevolodia <v.k.bickovs@student.tudelft.nl>
2025-05-18 19:51:08 +0200

    Some unlisted page added?

---
+++ src/pages/unlisted_brainfuck.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function UnlistedBrainfuck() {
+	return (
+		<div>
+
+			<br />
+			<br />
+
+			<iframe src="https://pastebin.com/embed_iframe/Taae81S2?theme=dark"
+			        style={{"border": "none", "borderRadius": "8px", "width": "100%", "height": "75vh"}}></iframe>
+
+		</div>
+	);
+}
+
+export default UnlistedBrainfuck;
