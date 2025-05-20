381fcf91b2e965f3a0e0b79973ddf2cc54617522
nevolodia <v.k.bickovs@student.tudelft.nl>
2025-05-20 13:57:21 +0200

    Contact page itself added.

---
+++ src/pages/contact.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from "../components/Image";
+
+function Contact() {
+	return (
+		<div>
+			<p>
+				Hi, happy to see you here!
+
+				<br />
+				<br />
+
+				My github: <br />
+				<a href={"https://github.com/nevolodia"}>
+					https://github.com/nevolodia
+				</a>
+
+				<br />
+				<br />
+
+				My official email: <br />
+				work [at] nevolodia [dot] com
+
+				<br />
+				<br />
+
+				My study email: <br />
+				v.k.bickovs [at] student [dot] tudelft [dot] nl
+
+				<br />
+				<br />
+
+				My personal email: <br />
+				me [at] nevolodia [dot] com
+			</p>
+		</div>
+	);
+}
+
+export default Contact;
