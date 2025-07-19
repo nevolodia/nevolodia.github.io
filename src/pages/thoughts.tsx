773c9653a5a6eec3a9b3672fe9222ad9576dd9d4
nevolodia <bolodiab3007@gmail.com>
2025-07-19 17:08:53 +0200

    Under development...

--- src/pages/thoughts.tsx
+++ src/pages/thoughts.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Link from "../components/Link";
 import titles from './thoughts/titles.json';
 
@@ -6,26 +7,10 @@ function Thoughts() {
     // if we have ?thought=n, we show the thought
     // oltherwise, we show the selection of thoughts
 
-    var content;
-    const thought = window.location.search.split('thought=')[1];
-
-    if (thought) {
-        const thoughtStr = thought.padStart(3, '0');
-        const thoughtText = require(`./thoughts/${thoughtStr}.json`);
-        content = <div>{thoughtText}</div>;
-    } else {
-        const titlesList = titles.map((title, index) => {
-            return <Link key={index} link={`?p=thoughts&thought=${title.number}`}> {title.title}</Link>;
-        });
-        content = titlesList.map((title, index) => {
-            return <>{index + 1}. {title}</>;
-        });
-    }
-
 	return (
 		<div>
             <p>
-                {content}
+                Under development...
             </p>
 		</div>
 	);
