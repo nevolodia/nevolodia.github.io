70d0f6e1afc42d99c782f695de9b4a1df4ef041a
nevolodia <bolodiab3007@gmail.com>
2025-09-04 10:12:41 +0200

    Backgroud animation and new fonts added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import _main from './pages/_main';
 import './css/index.css';
+import './css/fonts.css';
+
+import './css/background-animation.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
