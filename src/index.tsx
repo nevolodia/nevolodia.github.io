424280046567c0fc2cf869db43760ce9d5a95460
nevolodia <bolodiab3007@gmail.com>
2025-09-04 15:14:23 +0200

    Fonts added with npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import _main from './pages/_main';
-import './css/index.css';
-import './css/fonts.css';
 
+// Fonts
+import '@fontsource/abril-fatface';
+import '@fontsource/space-mono';
+import '@fontsource/roboto-mono';
+
+// Styles
 import './css/background-animation.css';
+import './css/index.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
