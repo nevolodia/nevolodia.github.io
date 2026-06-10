cd3133c83c25521561c548f11ecd1d449a05ccdc
nevolodia <bolodiab3007@gmail.com>
2026-06-10 02:08:01 +0200

    1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,56 +15,10 @@ function Home()
 
 				My name is Vladimir, and this is my website.
 
-				<br/>
-				<br/>
-
-				I like Computer Science, Mathematics, and some other things, you can find on these pages:
-
-				<br/>
-				<br/>
-
-				<Link link="?p=study" a_style={true}>
-					Study
-				</Link> & <Link link="?p=work" a_style={true}>
-					Work
-				</Link> &lt;- Where do I study? Where do I work?
-
-				<br/>
-				<br/>
-
-				Some achievements, like making Braif. compiler, or winning hackathons -&gt; <Link link="?p=achievements" a_style={true}>
-					Achievements
-				</Link>
-
-				<br/>
-				<br/>
-
-				Some blog posts of mine -&gt; <Link link="?p=thoughts" a_style={true}>
-					Thoughts
-				</Link>
-
-				<br/>
-				<br/>
-
-				<Link link="?p=gallery" a_style={true}>
-					Gallery
-				</Link> &lt;- Some photos I took
-
-				<br/>
-				<br/>
-
-				How to contact me? -&gt; <Link link="?p=contact" a_style={true}>
-					Contact
-				</Link>
-
-				<br/>
-				<br/>
-
 				<Image name="me.jpg" highFetchPriority={true}/>
 
 				<br/>
 
-				I use Arch btw;)
 			</p>
 		</div>
 	);
