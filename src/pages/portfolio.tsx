b166d4aafb920bf4a5f4779f27dc34d53565157d
nevolodia <bolodiab3007@gmail.com>
2026-06-10 03:16:25 +0200

    69

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -28,14 +28,14 @@ function Portfolio()
 				<br />
 				<br />
 
-				&gt; Lead organizer of the first-ever Latvian Open AI Olympiad; couching the national team to represent
-				Latvia at IOAI and CEOAI international olympiads. Work in collaboration with University of Latvia;
-				secured sponsorships from Jane Street, Pirm¯a Programm¯eˇsanas Skola, and SEB bank.
+				&gt; Lead organizer of the first-ever Latvian Open AI Olympiad; couching national team for IOAI & CEOAI.
+				<a href="https://omi.lv/" target="_blank">https://omi.lv/</a>
 
-				&gt; Founded and lead an AI education initiative focused on advancing AI literacy in Latvia and worldwide;
-				lead organizer of the Latvian Open AI Olympiad; host introductory AI lectures in schools.
+				<br />
+				<br />
+
+				&gt; Founder of non-profit developing AI education for students in Latvia and worldwide.
 				<a href="https://lemon-ai.org/" target="_blank">https://lemon-ai.org/</a>
-
 
 			</p>
 		</div>
